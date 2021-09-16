69787240a46d07d6459da518e386b831ef33d466
Eddy <eddymartin0701@gmail.com>
2021-09-16 16:06:21 +0200

    function

---
+++ js/exercice_function.js
@@ -0,0 +1,29 @@
+        console.log("\nexercice 1");
+
+Math.random();
+var random = (Math.random());
+
+console.log(random);
+
+        console.log("\nexercice 2");
+
+var number = (12.28956);
+    console.log(number);
+    // console.log(Math.round(number));
+// var round = (number.toFixed(2));
+    console.log(number.toFixed(2));
+
+        console.log("\nexercice 3");
+
+var age = ("35");
+        console.log(typeof age);
+
+var age = (parseFloat("35"));
+        console.log(typeof age);
+
+        console.log("\nexercice 4");
+
+
+
+
+
